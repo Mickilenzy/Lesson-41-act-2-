b637ddca989a96bc843dfe99ca78bf32a2fd791e
Mickilenzy <ilerioluwamichael297@gmail.com>
2025-02-24 11:41:26 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+setc1 = {"apple", "orange", "banana","pineapple","mango","raspberry"}
+setc2 = {"banana", "kiwi", "grapes", "mango","raspberry"}
+print("Original sets")
+print(setc1)
+print(setc2)
+setc = setc1.union(setc2)
+print("\nUnion of above sets:")
+print(setc)
